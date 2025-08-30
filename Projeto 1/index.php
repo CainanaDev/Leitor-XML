ef327dc1375cc27ce17a0a511a216c1c5b6011d7
CainanaDev <cainana10@gmail.com>
2025-08-30 11:16:49 -0300

    Adção das tag meta e Link Bootstrap, e Fontawesome. Além da configuração do Js para respectiva index

---
+++ Projeto 1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!--Font Awesome-->
+  <script src="https://kit.fontawesome.com/1b112f6203.js" crossorigin="anonymous"></script>
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/js.js"></script>
+  <title>Pagina Inicial</title>
+</head>
+
+<body>
+  <h1><em>Temos uma Pagina</em> inicial aqui</h1>
+  <h2>Temos uma Pagina inicial aqui</h2>
+  <h3>Temos uma Pagina inicial <u>aqui</u></h3>
+  <strike>
+    <h4>Temos uma Pagina inicial aqui</h4>
+  </strike>
+
+  <ul>
+    <li>Home</li>
+    <li>Fale Conosco</li>
+    <li>Dashboard</li>
+  </ul>
+
+</body>
+
+</html>
