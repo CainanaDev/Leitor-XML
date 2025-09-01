3736baaf3b9fb8a7b9aa0e5983027f974677b6bc
Cainana-Dev <cainana10@gmail.com>
2025-08-31 21:39:29 -0300

    Testes com JavaScript

--- Projeto 1/teste.php
+++ Projeto 1/teste.php
@@ -1,19 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
-  <form method="POST" action="teste2.php" enctype="multipart/form-data">
+  <!--
+  <form method="POST" action="teste.php" enctype="multipart/form-data">
     <div>
       <label for="file">Choose file to upload</label>
       <input type="file" id="file" name="file[]" multiple accept=".xml" />
     </div>
+
     <div>
-      <button>Submit</button>
+      <button onclick="mostrarArquivos()">Submit</button>
     </div>
   </form>
+
+  <div id="listaArquivos"></div>
+  -->
+  <input type="file" id="file" name="file[]" multiple accept=".xml" />
+  <div>
+    <button onclick="mostrarArquivos()">Submit</button>
+  </div>
+  <div id="listaArquivos"></div>
+
+
 </body>
+
+<script>
+  function mostrarArquivos() {
+    const input = document.getElementById('file');
+    const arquivos = input.files; // FileList
+    const lista = document.getElementById('listaArquivos');
+    lista.innerHTML = ''; // Limpa o conteúdo anterior
+
+    for (let i = 0; i < arquivos.length; i++) {
+      const item = document.createElement('p');
+      item.textContent = `Arquivo ${i + 1}: ${arquivos[i].name} (${arquivos[i].type}, ${arquivos[i].size} bytes)`;
+      lista.appendChild(item);
+    }
+    console.log(arquivos)
+  }
+</script>
+
 </html>
--- Projeto 1/teste2.php
+++ Projeto 1/teste2.php
@@ -1,15 +1,18 @@
 <?php
 
+use Dom\Document;
 
-$a ='';
+$a = '';
 $xml = new DOMdocument();
-$a = $_FILES['file'] ['tmp_name'];
+$a = $_FILES['file']['tmp_name'];
 
+
+/*
 
 echo '<pre>';
 print_r($a);
 echo '<pre/>';
-
+*/
 
 echo '<pre>';
 print_r($xml);
@@ -17,13 +20,15 @@ echo '<pre/>';
 
 
 
-$xml->load($a); // Carrega o XML
-
+//$xml->load($a); // Carrega o XML
+/*
 // Aqui você pode visualizar o conteúdo
 echo "<pre>" . htmlentities($xml->saveXML()) . "</pre>";
-
-
-
-
+*/
 
 ?>
+
+<script>
+  let a = document.getElementById("file").files
+  console.log('a')
+</script>
