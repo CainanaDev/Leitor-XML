235e0cd27ac9c8a184efbebb323d3d226aef720f
CainanaDev <cainana10@gmail.com>
2025-10-29 15:54:58 -0300

    Adção do Jquery.

--- Leitor-XML/index.php
+++ Leitor-XML/index.php
@@ -6,11 +6,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
   <!--Namalize CSS-->
   <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
 
   <!--Font Awesome-->
   <script src="https://kit.fontawesome.com/1b112f6203.js" crossorigin="anonymous"></script>
+
+  
   
 
   <!-- Custom CSS -->
@@ -18,9 +27,10 @@
   <link rel="stylesheet" type="text/css" href="css/midia.css">
 
   <title>
-    Pagina Inicial
+    Pagina Inicial - v1.1
   
   </title>
+  
 </head>
 
 <body id="home">
@@ -31,7 +41,7 @@
         XML<span class="font-italic">Analyse</span>
       </div>
       <div  id="texto">
-        <h2 class="font-weight-bold text-uppercase">Leitor XML</h2>
+        <h2 class="font-weight-bold text-uppercase">Leitor XML - v1.1</h2>
       </div>
       <div  id="cadastro">
         <a class="btn btn-light" href="#">Login</a> <span id="marcador">|</span>
@@ -119,15 +129,12 @@
     <p>&copy; 2025 - Todos os direitos reservados</p>
   </footer>
 
-  <script src="js/js.js"></script>
+  
   <!-- JavaScript (Opcional) -->
-  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
 
 </body>
 
-
+<script src="js/js.js"></script>
 
 </html>
