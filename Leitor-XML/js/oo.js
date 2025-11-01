cf87271408ca82d6f5f9d9f4766c7866924cb9e0
CainanaDev <cainana10@gmail.com>
2025-11-01 11:16:30 -0300

    Implementada toda logica de parser, e atribuição dinamica para nossas class

--- Leitor-XML/js/oo.js
+++ Leitor-XML/js/oo.js
@@ -19,7 +19,8 @@ $(document).ready(()=>{
     //Ler o retorno de FileRead como string
     lerXML(_xmlString){
       this._xmlString = _xmlString;
-      console.log(_xmlString)
+      
+      //console.log(this._xmlString)
     };
     parserXml() {
       if(!this._xmlString){
@@ -27,15 +28,58 @@ $(document).ready(()=>{
       };
       const parser = new DOMParser()
       this._parsedXml = parser.parseFromString(this._xmlString, 'text/xml')
-      console.log(_parsedXml)
+      
+      
+      //Povoando as informações do objeto com os dados dos arquivos analisados
+      this._cnpj = this._parsedXml.querySelector("CNPJ")?.textContent || this._cnpj;
+      this.chave = this._parsedXml.querySelector("chNFe")?.textContent || this.
+      chave;
+      this.dataEmissao = this._parsedXml.querySelector("dhEmi")?.textContent || this.dataEmissao;
+      this.naturaOp = this._parsedXml.querySelector("natOp")?.textContent || this.naturaOp;
+      this.modelo = this._parsedXml.querySelector("mod")?.textContent || this.modelo;
+      this.valor = this._parsedXml.querySelector("vNF")?.textContent || this.valor;
+      this.status = this._parsedXml.querySelector("cStat")?.textContent || this.status;
+      this.nfeNumero = this._parsedXml.querySelector("nNF")?.textContent || this.nfeNumero;
+      this.numSerie = this._parsedXml.querySelector("serie")?.textContent || this.numSerie;
+      
+
     };
 
     verficaCancelado(chave) {
     }; 
-  };
+  }; 
+
+  const input = $('#file')[0].files //FileList
+
+  for (let i= 0; i < input.length; i++) {
+    
+    const reader = new FileReader() //leitura do Arquivo formato texto.
+    let xml = new Xml() //a cada interação do laço, um novo objeto XML é criado 
+    reader.onload = (event) => {
+      
+      const xmlString = event.target.result    
+        xml.lerXML(xmlString)
+        xml.parserXml()
+        console.log(xml.chave)
+        //console.log(xml)
+    }; reader.readAsText(input[i]) //arquivo a ser lido
+    
+    
+  }
+
+ 
+
+
   
-  let xml = new Xml()
-  console.log(xml.parserXml())
+  
+  
+
+
+
+
+
+ 
+
 
 });
 
