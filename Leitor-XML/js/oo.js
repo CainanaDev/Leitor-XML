f27c5fb8b9c727da6a9224ac670f5b3514f59e75
CainanaDev <cainana10@gmail.com>
2025-11-18 17:50:42 -0300

    Ajustes feito no trabalho: Metodo valida dados para debug. Além da implementação do desestruturação do array dados

--- Leitor-XML/js/oo.js
+++ Leitor-XML/js/oo.js
@@ -17,8 +17,8 @@ $(document).ready(()=>{
 
     }
     //Ler o retorno de FileRead como string
-    lerXML(_xmlString){
-      this._xmlString = _xmlString;
+    lerXML(xmlString){
+      this._xmlString = xmlString  ;
       
       //console.log(this._xmlString)
     };
@@ -81,7 +81,7 @@ $(document).ready(()=>{
     
     //retorna as informações do objeto em questão em formato de array.
     toTableRow() {
-      return [
+      return  [
         this._cnpj,
         this.chave,
         this.dataEmissao, 
@@ -94,6 +94,12 @@ $(document).ready(()=>{
       ]
     };
 
+    validaDados(){
+      for(let i in this){
+        console.log(i, this[i])
+      }
+    }
+
     
   }; 
 
@@ -102,22 +108,33 @@ $(document).ready(()=>{
   const arquivos = $('#file')[0].files //FileList
   
   //Loop para leitura dos arquivos carregados
-  for (let i= 0; i < arquivos.length; i++) {
+  for (let i = 0; i < arquivos.length; i++) {
     //Instancia de FeleReader que transforma o arquivo carregado em texto bruto
     const reader = new FileReader() //leitura do Arquivo formato texto.
     let xml = new Xml() //a cada interação do laço, um novo objeto XML é criado 
     reader.onload = (event) => {
       
-      const xmlString = event.target.result    
+        const xmlString = event.target.result    
         xml.lerXML(xmlString)
         xml.parserXml()
         xml.verficaCancelado(xml.chave, xml.status)
 
         console.log('Chave do XML: '+ xml.chave)
         console.log('Codigo do Status: '+ xml.status)
-        console.log('--------------')
        
-        console.log(xml.toTableRow())
+        
+        let [ //desestruturação do array com as informaçoes do objeto
+          cnpj, 
+          chave, 
+          data, 
+          op, 
+          valor, 
+          modelo,
+          status,
+          n,
+          s
+        ] = xml.toTableRow()
+        
     }; reader.readAsText(arquivos[i]) //arquivo a ser lido
     
     
